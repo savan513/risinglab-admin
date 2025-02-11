3e204aa9353afca20a0048021f2723d18e741929
Anshit Kabariya <70514612+Nidhi-Vedaha@users.noreply.github.com>
2025-02-11 23:32:41 +0530

    Disable no-unused-vars ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     'lines-around-comment': [
       'error',
