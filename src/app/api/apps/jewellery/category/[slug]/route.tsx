1af69bc06c70dc82739e9def01460ddcfe5a3017
Anshit Kabariya <70514612+Nidhi-Vedaha@users.noreply.github.com>
2025-02-12 09:22:24 +0530

    Refactor jewellery category route with updated type definition and parameter handling

--- src/app/api/apps/jewellery/category/[slug]/route.tsx
+++ src/app/api/apps/jewellery/category/[slug]/route.tsx
@@ -1,42 +1,33 @@
-import type { NextRequest } from 'next/server';
+import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-import { Jewellery } from "@/data/model/jewellery.schema"
-import { Category } from "@/data/model/category.schema"
+import { Jewellery } from '@/data/model/jewellery.schema'
+import { Category } from '@/data/model/category.schema'
 
-export async function GET(
-    request: NextRequest,
-    { params }: { params: { slug: string } }
-) {
-    try {
-        const slug = await params.slug;
+type tParams = Promise<{ slug: string[] }>
 
-        console.log("slug :==> ", slug);
+export async function GET(request: NextRequest, { params }: { params: tParams }) {
+  try {
+    const { slug } = await params
 
-        // First find the category by slug
-        const category = await Category.findOne({ slug: slug })
+    console.log('slug :==> ', slug)
 
-        console.log("category :==> ", category);
+    // First find the category by slug
+    const category = await Category.findOne({ slug: slug })
 
-        if (!category) {
-            return NextResponse.json(
-                { message: 'Category not found' },
-                { status: 404 }
-            )
-        }
+    console.log('category :==> ', category)
 
-        // Then find all jewellerys that belong to this category
-        const Jewellerys = await Jewellery.find({ category: category._id })
-            .populate('category')
-            .sort({ createdAt: -1 })
-
-        return NextResponse.json(Jewellerys)
-    } catch (error) {
-        console.error('Error fetching jewellerys by category:', error)
-
-        return NextResponse.json(
-            { message: 'Internal server error' },
-            { status: 500 }
-        )
+    if (!category) {
+      return NextResponse.json({ message: 'Category not found' }, { status: 404 })
     }
-} 
+
+    // Then find all jewellerys that belong to this category
+    const Jewellerys = await Jewellery.find({ category: category._id }).populate('category').sort({ createdAt: -1 })
+
+    return NextResponse.json(Jewellerys)
+  } catch (error) {
+    console.error('Error fetching jewellerys by category:', error)
+
+    return NextResponse.json({ message: 'Internal server error' }, { status: 500 })
+  }
+}
